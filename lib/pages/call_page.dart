14d4e015917b63e23872eb0eca6dc2e61ecb872e
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-07-20 00:01:56 +0530

    Create call_page.dart

---
+++ lib/pages/call_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+class CallPage extends StatelessWidget {
+  const CallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('CallPage'),
+    );
+  }
+}
