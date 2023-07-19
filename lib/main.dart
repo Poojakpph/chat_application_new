e384068b9383ad08b38663d7fbf4e3f10d86d0ed
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-07-19 23:57:39 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chat_app/screens/home_screen.dart';
+import 'package:chat_app/screens/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() =>runApp(const MyApp());
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}): super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.light(),
+      darkTheme: AppTheme.dark(),
+      themeMode: ThemeMode.dark,
+
+      title: 'Chatter',
+      home: HomeScreen()
+        
+      );
+    }
+}
